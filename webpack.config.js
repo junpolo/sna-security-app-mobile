c3444fc526fa1cffcac9514571dd8aa2c4fcd637
Jun Polo <junpolojr@gmail.com>
2023-09-06 13:43:42 +0800

    fix: build issues

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,6 @@ const webpack = require("@nativescript/webpack");
 module.exports = (env) => {
   webpack.init(env);
 
-  // Add the 'nativescript-auth0' module to the appComponents
-  webpack.Utils.addCopyRule({
-    from: "node_modules/nativescript-auth0/platforms/android/src/main/java/org/nativescript/auth0/",
-    to: "bundle/app/tns_modules/nativescript-auth0/android/provider/",
-  });
-
   // Learn how to customize:
   // https://docs.nativescript.org/webpack
 
